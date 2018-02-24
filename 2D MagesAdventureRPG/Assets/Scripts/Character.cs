d36d74be9008d800f0629bd44c8a07f2a7ba36fd
IanMunster <I.Munster92@Gmail.com>
2018-02-24 04:53:13 +0100

    2DMARPG Upload 1

---
+++ 2D MagesAdventureRPG/Assets/Scripts/Character.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Character.
+/// 
+/// </summary>
+
+public abstract class Character : MonoBehaviour {
+
+
+	// Movement speed
+	[SerializeField] private float speed;
+	// Direction of Movement
+	protected Vector2 direction;
+
+	// Animator Component for Movement, in Child object "Sprite"
+	private Animator anim;
+
+
+	// Use this for initialization (beforeStart)
+	protected virtual void Awake () {
+		//
+		anim = GetComponent <Animator> ();
+	}
+
+
+	// Update is called once per frame
+	protected virtual void Update () {
+		//Call the MoveFunction
+		Move ();
+		AnimateMovement ();
+	}
+
+
+	// Function to Move
+	public void Move () {
+		// Translate the Transform to Direction, with Speed per Frame
+		transform.Translate (direction * speed * Time.deltaTime);
+	}
+
+
+	//Function to Animate the Movement
+	public void AnimateMovement () {
+		anim.SetFloat("DirX", direction.x);
+		anim.SetFloat ("DirY", direction.y);
+	}
+
+}
---
+++ 2D MagesAdventureRPG/Assets/Scripts/Player.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Player.
+/// 
+/// </summary>
+
+public class Player : Character {
+
+
+
+	// StatClass, Health of Player
+	[SerializeField] private Stat health;
+	// StatClass, Mana of Player
+	[SerializeField] private Stat mana;
+
+
+	// Current value of Health
+	private float healthValue = 100f;
+	// Max value of Health
+	/*[SerializeField]*/ private float maxHealth = 100f;
+
+	// Current value of Mana
+	private float manaValue = 100f;
+	// Max value of Mana
+	private float maxMana = 200f;
+
+
+	// Use this for initialization (Before Start)
+	protected override void Awake () {
+		
+		health.Initialize (healthValue, maxHealth);
+		mana.Initialize (manaValue, maxMana);
+
+		base.Awake ();
+	}
+
+
+	// Update is called once per frame
+	protected override void Update () {
+		GetInput ();
+		base.Update ();
+	}
+
+
+	// Function to get Input
+	private void GetInput () {
+		// Story the input
+		direction.y = Input.GetAxisRaw ("Vertical");
+		direction.x = Input.GetAxisRaw ("Horizontal");
+		// Normalize to remove diagonally move bonus
+		direction.Normalize ();
+
+		/// Debug
+		/// 
+		if (Input.GetKeyDown(KeyCode.I)) {
+			health.MyCurrentValue += 10;
+			mana.MyCurrentValue += 20;
+		} 
+		if (Input.GetKeyDown(KeyCode.O)) {
+			health.MyCurrentValue -= 10;
+			mana.MyCurrentValue -= 20;
+		}
+		///
+		/// End Debug
+	}
+}
+
+
+
+
+
+
+
+
+
+
+// Values for Long Idle Animation
+//[SerializeField] private float waitLongMax = 600f;
+//private float waitLong;
+/*
+	 // Function to Override Character Animate? And Play Long Idle Animation
+protected override void AnimateMovement () {
+	base.AnimateMovement ();
+	//
+	if (direction == Vector2.zero) {
+		waitLong++;
+		if (waitLong == waitLongMax) {
+			//.SetTrigger ("LongIdle");
+			waitLong = 0;
+		}
+	} else {
+		waitLong = 0;
+	}
+}
+*/
